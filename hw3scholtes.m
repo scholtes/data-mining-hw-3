337b1ec4672a4365131d7aad56f6d77ad17345c2
scholtes <scholtgt@mail.uc.edu>
2016-11-09 17:02:46 -0500

    Part 1.a-d complete

--- hw3scholtes.m
+++ hw3scholtes.m
@@ -26,6 +26,7 @@ data = data(~any(isnan(data),2), :);
 k_values = 3:8;
 sse = [];
 silhouettes = [];
+clusterings = cell(0);
 for k = k_values
     best_sse = Inf;
     for i = 1:3
@@ -50,17 +51,31 @@ for k = k_values
     sse(end+1) = best_sse;
     % Silhouettes
     silhouettes(end+1) = mean(silhouette(data, idx, 'Euclidean'));
+    % Save clusterings for later use
+    clusterings(end+1) = {struct('idx',idx,'C',C,'sumd',sumd,'D',D)};
 end
 
-% Plot SSE versus k
+% 1.a) Plot SSE versus k
 figure;
-plot(k_values, sse, 'b*');
+plot(k_values, sse, 'r*');
 title('SSE versus k (number of clusters)');
 xlabel('k-value (number of clusters)');
 ylabel('Sum of Squared Errors (square distance)');
 
+% 1.b) Plot Silhouette versus k
 figure;
 plot(k_values, silhouettes, 'k*');
 title('Silhouette versus k (number of clusters)');
 xlabel('k-value (number of clusters)');
 ylabel('Silhouette (distance)');
+
+% "Best" clustering choice  
+% We will use silhouette/sse as a metric we wish to maximize
+best_metric = silhouettes./sse;
+[~, best_idx] = max(best_metric);
+%%% 1.c) Best k-value %%%
+best_k = k_values(best_idx);
+%%% 1.d) Best clustering %%%
+% Note: best_clustering.C is the centroids of the "best" clustering
+best_clustering = clusterings{best_idx};
+
